6ff3f3fcc32fcd9a21651cc73b318efe36ae0f75
Zubair Ahmad <zubairahmadbjr7@gmail.com>
2026-07-20 23:33:16 +0500

    Initial AI Career Hub project setup

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("AI Career Hub Started Successfully!");
+
+const searchButton = document.querySelector(".btn-primary");
+
+searchButton.addEventListener("click", function () {
+
+    alert("Search feature will be added in Module 2.");
+
+});
